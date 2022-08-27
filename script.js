ef6117e70e219eac6767f7c7e8d40c6d9824a4e1
Amin <107203714+0AmIn3@users.noreply.github.com>
2022-08-27 17:04:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let data = document.querySelectorAll('div[data-metod]')
 
 narost.onclick = () => {
     count = Number(chislo.innerHTML)
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i <= 10000; i++) {
         setTimeout(() => {
 
             chislo.innerHTML = count + i
@@ -21,7 +21,7 @@ narost.onclick = () => {
 }
 ubiv.onclick = () => {
     count = Number(chislo.innerHTML)
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i <= 10000; i++) {
         setTimeout(() => {
             chislo.innerHTML = count - i
         }, 200 * i)
@@ -35,41 +35,42 @@ stop.onclick = () => {
     chislo.innerHTML = count
 }
 
-data.forEach(btn => {
-    btn.onclick = () => {
-        let met = btn.getAttribute('data-metod')
-        switch (met) {
-            case '-':
-                ubiv.onclick = () => {
-                    count = Number(chislo.innerHTML)
-                    for (let i = 0; i <= 10; i++) {
-                        setTimeout(() => {
-                            chislo.innerHTML = count - i
-                        }, 200 * i)
+// data.forEach(btn => {
+//     btn.onclick = () => {
+//         let met = btn.getAttribute('data-metod')
+//         switch (met) {
+//             case '-':
+//                 ubiv.onclick = () => {
+//                     count = Number(chislo.innerHTML)
+//                     for (let i = 0; i <= 10; i++) {
+//                         setTimeout(() => {
+//                             chislo.innerHTML = count - i
+//                         }, 200 * i)
 
-                    }
-                }
-                break;
-            case '+':
-                narost.onclick = () => {
-                    count = Number(chislo.innerHTML)
-                    for (let i = 0; i <= 10; i++) {
-                        setTimeout(() => {
-                            chislo.innerHTML = count + i
-                        }, 200 * i)
+//                     }
+//                 }
+//                 break;
+//             case '+':
+//                 narost.onclick = () => {
+//                     count = Number(chislo.innerHTML)
+//                     for (let i = 0; i <= 10; i++) {
+//                         setTimeout(() => {
+//                             chislo.innerHTML = count + i
+//                         }, 200 * i)
 
-                    }
+//                     }
 
-                }
-                break;
-            case 'STOP':
-                stop.onclick = () => {
-                    count = Number(chislo.innerHTML)
-                    chislo.innerHTML = count
-                }
-                break;
+//                 }
+//                 break;
+//             case 'STOP':
+//                 stop.onclick = () => {
+//                     count = Number(chislo.innerHTML)
+//                     chislo.innerHTML = count
+//                 }
+//                 break;
 
-        }
-    }
-})
+//         }
+//     }
+// })
+
 
