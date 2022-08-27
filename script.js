3726bcc491f511a381e2777dc3c0ad8bc367d88a
0AmIn3 <aminfedya2888@gmail.com>
2022-08-27 13:19:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+let ubiv = document.querySelector('.ubiv')
+let narost = document.querySelector('.narost')
+let stop = document.querySelector('.stop')
+let chislo = document.querySelector('.chislo')
+let count = 0
+
+let data = document.querySelectorAll('div[data-metod]')
+
+
+
+narost.onclick = () => {
+    count = Number(chislo.innerHTML)
+    for (let i = 0; i <= 10; i++) {
+        setTimeout(() => {
+
+            chislo.innerHTML = count + i
+        }, 200 * i)
+
+    }
+
+}
+ubiv.onclick = () => {
+    count = Number(chislo.innerHTML)
+    for (let i = 0; i <= 10; i++) {
+        setTimeout(() => {
+            chislo.innerHTML = count - i
+        }, 200 * i)
+
+    }
+
+}
+
+stop.onclick = () => {
+    count = Number(chislo.innerHTML)
+    chislo.innerHTML = count
+}
+
+data.forEach(btn => {
+    btn.onclick = () => {
+        let met = btn.getAttribute('data-metod')
+        switch (met) {
+            case '-':
+                ubiv.onclick = () => {
+                    count = Number(chislo.innerHTML)
+                    for (let i = 0; i <= 10; i++) {
+                        setTimeout(() => {
+                            chislo.innerHTML = count - i
+                        }, 200 * i)
+
+                    }
+                }
+                break;
+            case '+':
+                narost.onclick = () => {
+                    count = Number(chislo.innerHTML)
+                    for (let i = 0; i <= 10; i++) {
+                        setTimeout(() => {
+                            chislo.innerHTML = count + i
+                        }, 200 * i)
+
+                    }
+
+                }
+                break;
+            case 'STOP':
+                stop.onclick = () => {
+                    count = Number(chislo.innerHTML)
+                    chislo.innerHTML = count
+                }
+                break;
+
+        }
+    }
+})
+
